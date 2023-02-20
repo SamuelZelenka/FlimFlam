2d67ffcae5a5443969e76fc035b682297d1bed48
Diddedidde <didrikgrahn@gmail.com>
2023-02-20 22:00:07 +0100

    Added goblin movement

---
+++ Assets/Scripts/GoblinMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class GoblinMovement : MonoBehaviour
+{
+
+    Transform target;
+    Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float goblinMovementSpeed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        moveTowardsTarget(target);
+    }
+
+
+    private void moveTowardsTarget(Transform target){
+
+        Vector3 nextPosition = (transform.position - target.position).normalized;
+
+         myRigidBody.MovePosition(transform.position - nextPosition * Time.deltaTime * goblinMovementSpeed);
+    }
+}
