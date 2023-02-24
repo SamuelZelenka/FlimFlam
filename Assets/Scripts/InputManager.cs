12cdc6b71230a7fdfe9b98d24c4ff13f0ab7c368
Samuel Zelenka af Rolén <samuel.zelenka@hotmail.com>
2023-02-24 23:19:26 +0100

    Updated player movement with new input system

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,13 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class InputManager
+public class InputManager : MonoBehaviour
 {
     private static HashSet<InputBase> _inputs = new HashSet<InputBase>();
+    private static InputManager _instance;
+    public static InputManager Instance => _instance;
 
-    public static void RegisterKey(KeyCode key, Action action)
+    [RuntimeInitializeOnLoadMethod]
+    public static void InitializeObject()
     {
-        _inputs.Add(new InputKey(key, action));
+        _instance = new GameObject().AddComponent<InputManager>();
+        _instance.name = "InputManager";
+    }
+
+    public void Update()
+    {
+        foreach (var input in _inputs)
+        {
+            input.CheckInput();
+        }
+    }
+
+    public static void RegisterKey(InputKeyState keyState , Action action, params KeyCode[] keycodes)
+    {
+        foreach (var key in keycodes)
+        {
+            _inputs.Add(new InputKey(key, keyState, action));
+        }
     }
 
     public static void RegisterAxis(string key, Action<float> action)
@@ -20,28 +40,47 @@ public static class InputManager
 
 public abstract class InputBase
 {
-    public abstract void Update();
+    public abstract void CheckInput();
 }
 
 public class InputKey : InputBase
 {
     private KeyCode _key;
+    private InputKeyState _inputKeyState;
     private event Action _action;
 
-    public InputKey(KeyCode key, Action action)
+    public InputKey(KeyCode key, InputKeyState inputKeyState, Action action)
     {
         _key = key;
         _action = action;
+        _inputKeyState = inputKeyState;
     }
 
-    public override void Update()
+    public override void CheckInput()
     {
-        _action?.Invoke();
+        switch (_inputKeyState)
+        {
+            case InputKeyState.KeyHold when Input.GetKey(_key):
+            case InputKeyState.KeyDown when Input.GetKeyDown(_key):
+            case InputKeyState.KeyUp when Input.GetKeyUp(_key):
+                _action?.Invoke();
+                return;
+            default:
+                break;
+        }
     }
+}
+
+public enum InputKeyState
+{
+    KeyHold,
+    KeyDown,
+    KeyUp
 }
 
 public class InputAxis : InputBase
 {
+    private float _lastValue;
     private string _axis;
     private Action<float> _action;
 
@@ -50,9 +89,15 @@ public class InputAxis : InputBase
         _axis = axis;
         _action = action;
     }
-    public override void Update()
-    {
-        _action?.Invoke(Input.GetAxis(_axis));
-    }
 
+    public override void CheckInput()
+    {
+        var value = Input.GetAxis(_axis);
+        if (value != _lastValue)
+        {
+            _action?.Invoke(_lastValue);
+        }
+        _lastValue = value;
+
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,11 +8,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private int movementSpeed = 1;
 
+    private Vector3 nextPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+
+        InputManager.RegisterKey(InputKeyState.KeyHold, MoveUp, KeyCode.UpArrow, KeyCode.W);
+        InputManager.RegisterKey(InputKeyState.KeyHold, MoveDown, KeyCode.DownArrow, KeyCode.S);
+        InputManager.RegisterKey(InputKeyState.KeyHold, MoveRight, KeyCode.RightArrow, KeyCode.D);
+        InputManager.RegisterKey(InputKeyState.KeyHold, MoveLeft, KeyCode.LeftArrow, KeyCode.A);
     }
+    public void MoveUp() => nextPosition += Vector3.up;
+    public void MoveDown() => nextPosition += Vector3.down;
+    public void MoveRight() => nextPosition += Vector3.right;
+    public void MoveLeft() => nextPosition += Vector3.left;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -22,35 +31,10 @@ public class PlayerMovement : MonoBehaviour
         move();
     }
 
-    public void move(){
-
-        Vector3 nextPosition = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.UpArrow)){
-            
-            nextPosition += new Vector3(0, 1, 0);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow)){
-
-            nextPosition += new Vector3(0, -1, 0);
-
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow)){
-
-            nextPosition += new Vector3(1, 0, 0);
-
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow)){
-
-            nextPosition += new Vector3(-1, 0, 0);
-
-        }
-
-        nextPosition = nextPosition.normalized;
-
-        myRigidBody.MovePosition(transform.position + nextPosition * Time.deltaTime * movementSpeed);
+    public void move()
+    {
+        nextPosition = transform.position + (nextPosition.normalized * Time.deltaTime * movementSpeed);
+        myRigidBody.MovePosition(nextPosition);
+        nextPosition = Vector3.zero;
     }
 }
